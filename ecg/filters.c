e6d72094d2b6b218e4315bcdc0c75dbfad9adebc
Markus Færevaag <mafaer@gmail.com>
2013-09-11 17:21:15 +0200

    Added faulty window filter

--- ecg/filters.c
+++ ecg/filters.c
@@ -1,21 +1,21 @@
 #include "filters.h"
 
 int apply_low_pass(int sig_data[], int filter_data[],
-		    int sig_len, int filter_len) {
+                   int sig_len, int filter_len) {
 
     int val_y = 0;
     if (filter_len >= 1)
-	val_y += 2 * filter_data[filter_len - 1];
+        val_y += 2 * filter_data[filter_len - 1];
     if (filter_len >= 2)
-	val_y -= filter_data[filter_len - 2];
+        val_y -= filter_data[filter_len - 2];
 
     int val_x = 0;
     if (sig_len >= 1)
-	val_x += sig_data[sig_len - 1];
+        val_x += sig_data[sig_len - 1];
     if (sig_len >= 7)
-	val_x -= 2 * sig_data[sig_len - 7];
+        val_x -= 2 * sig_data[sig_len - 7];
     if (sig_len >= 13)
-	val_x += sig_data[sig_len - 13];
+        val_x += sig_data[sig_len - 13];
 
     val_x *= 1.0 / 32.0;
 
@@ -23,42 +23,54 @@ int apply_low_pass(int sig_data[], int filter_data[],
 }
 
 int apply_high_pass(int sig_data[], int filter_data[],
-		   int sig_len, int filter_len) {
+                    int sig_len, int filter_len) {
 
     int val_y = 0;
     if (filter_len >= 1)
-	val_y += filter_data[filter_len - 1];
+        val_y += filter_data[filter_len - 1];
 
     int val_x = 0;
     if (sig_len >= 1)
-	val_x -= sig_data[sig_len - 1] / 32;
+        val_x -= sig_data[sig_len - 1] / 32;
     if (sig_len >= 17)
-	val_x += sig_data[sig_len - 17];
+        val_x += sig_data[sig_len - 17];
     if (sig_len >= 18)
-	val_x -= sig_data[sig_len - 18];
+        val_x -= sig_data[sig_len - 18];
     if (sig_len >= 33)
-	val_x += sig_data[sig_len - 33] / 32;
+        val_x += sig_data[sig_len - 33] / 32;
 
     return val_y + val_x;
 }
 
 int apply_derivative(int sig_data[], int filter_data[], int sig_len, int filter_len) {
-	int val_x = 0;
-	if (sig_len >= 1)
-		val_x += 2 * sig_data[sig_len - 1];
-	if (sig_len >= 2)
-		val_x += sig_data[sig_len - 2];
-	if (sig_len >= 4)
-		val_x -= sig_data[sig_len - 4];
-	if (sig_len >= 5)
-		val_x -= 2 * sig_data[sig_len - 5];
+    int val_x = 0;
+    if (sig_len >= 1)
+        val_x += 2 * sig_data[sig_len - 1];
+    if (sig_len >= 2)
+        val_x += sig_data[sig_len - 2];
+    if (sig_len >= 4)
+        val_x -= sig_data[sig_len - 4];
+    if (sig_len >= 5)
+        val_x -= 2 * sig_data[sig_len - 5];
 
-	val_x *= 1.0 / 8.0;
+    val_x *= 1.0 / 8.0;
 
-	return val_x;
+    return val_x;
 }
 
 int apply_square(int sig_data[], int filter_data[], int sig_len, int filter_len) {
     if (sig_len >= 1)
-		return sig_data[sig_len - 1] * sig_data[sig_len - 1];
+        return sig_data[sig_len - 1] * sig_data[sig_len - 1];
+    return 0;
+}
+
+int apply_window(int sig_data[], int filter_data[], int sig_len, int filter_len) {
+    int sum = 0;
+    int count = sig_len < 31 ? sig_len : 31;
+
+    for (int i = 1; i <= count; i++) {
+	sum += sig_data[sig_len - i];
+    }
+
+    return sum / count;
 }
--- ecg/main.c
+++ ecg/main.c
@@ -11,13 +11,13 @@ int main() {
 	int max = 10000;
 	int idx = 0;
 
-	int sig[max], filt_low[max], filt_high[max], filt_der[max], filt_sqr[max];
+	int sig[max], filt_low[max], filt_high[max], filt_der[max], filt_sqr[max], filt_win[max];
 	do {
 		sig[idx] = get_next_data();
 		printf("%4i", sig[idx]);
 
 		filt_low[idx] = apply_low_pass(sig, filt_low, idx + 1, idx);
-		printf(" -> %5i", filt_low[idx]);
+		printf(" -> %8i", filt_low[idx]);
 
 		filt_high[idx] = apply_high_pass(filt_low, filt_high, idx + 1, idx);
 		printf(" -> %5i", filt_high[idx]);
@@ -26,7 +26,10 @@ int main() {
 		printf(" -> %5i", filt_der[idx]);
 
 		filt_sqr[idx] = apply_square(filt_der, filt_sqr, idx + 1, idx);
-		printf(" -> %5i\n", filt_sqr[idx]);
+		printf(" -> %5i", filt_sqr[idx]);
+
+		filt_win[idx] = apply_window(filt_sqr, filt_win, idx + 1, idx);
+		printf(" -> %i\n", filt_win[idx]);
 	} while (++idx < max);
 
 	destroy_sensor();

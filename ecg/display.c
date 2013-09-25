8231f73b3bc6c77721c7c0a589e1973b8c35fb08
Simon Altschuler <simon@altschuler.dk>
2013-09-25 23:29:54 +0200

    simple pulse visualization via ncurses

--- ecg/display.c
+++ ecg/display.c
@@ -3,6 +3,7 @@
 #ifndef _DISPLAY_C_
 #define _DISPLAY_C_
 
+
 void init_display() {
 	initscr();
 }
@@ -12,6 +13,12 @@ void destroy_display() {
 }
 
 void update_display(float time, int mwi, int rpk, int pk, float pulse) {	
+	// add mwi to local buffer
+	prepend_array_int(mwi_buffer, 100, mwi);
+
+	// clear screen
+	erase();
+	
 	// labels
 	mvprintw(1, 1, "Time:");
 	mvprintw(2, 1, "MWI:");	
@@ -25,6 +32,16 @@ void update_display(float time, int mwi, int rpk, int pk, float pulse) {
 	mvprintw(3, 15, "%5i", rpk);	
 	mvprintw(4, 15, "%5i", pk);	
 	mvprintw(5, 15, "%5.1f", pulse);
+
+	// graph
+	int y_max = 1000;
+	//int y_min = -100;
+	// top right bottom left
+	int box[4] = {1, 400, 30, 20};
+	int xs = 100;
+	for (int i = 0; i < xs; i++) {
+		mvaddch(box[0] + box[2] - (mwi_buffer[i] / 200.0), box[3] + i, '*');
+	}
 
 	move(0,0);
 
--- ecg/display.h
+++ ecg/display.h
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 #include <ncurses.h>
 
+#include "array_utils.h"
+
 #ifndef _DISPLAY_H_
 #define _DISPLAY_H_
+
+static int mwi_buffer[100];
 
 void init_display();
 void destroy_display();

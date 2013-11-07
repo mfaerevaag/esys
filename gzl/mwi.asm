b8d474fd733e99712d72ad7801d36a75e32b7cce
Simon Altschuler <simon@altschuler.dk>
2013-11-07 17:19:35 +0100

    aowiijdaowdj

--- gzl/mwi.asm
+++ gzl/mwi.asm
@@ -1,22 +1,19 @@
-init:	
-	    #set %iggdx, $0
-        #set %idx, $0
-        #jmp begin
-begin:
-	    set %cadr, $0
-        set %acc, $0
-	    set %tmp, $31
-        add %eadr, %sadr, %tmp
+begin:	
+	    set $cadr, 0
+        set $acc, 0	
+	    #set $tmp, 31
+	    set $tmp, 31		
+        add $eadr, $sadr, $tmp
 loop:
-	    load %tmp, %cadr
-        add %acc, %acc, %tmp
-        set %tmp, $1
-        add %cadr, %cadr, %tmp
-        cmp %cadr, %eadr
+	    load $tmp, $cadr
+        add $acc, $acc, $tmp
+        set $tmp, 1
+        add $cadr, $cadr, $tmp
+        cmp $cadr, $eadr
         jlt loop
 average:
-	    set %tmp, $1118482 # div const for 30
-	    div %res, %acc, %tmp
-        set %tmp, $1
-        add %sadr, %sadr, %tmp
-        #jmp begin
+	    set $tmp, 1118482 # div const for 30
+	    div $res, $acc, $tmp
+        set $tmp, 1
+        add $sadr, $sadr, $tmp
+        set $tmp, 3

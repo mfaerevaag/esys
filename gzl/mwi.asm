d227a7be301187f11ec6264f48f28cf1e3ff124d
Simon Altschuler <simon@altschuler.dk>
2013-11-09 21:25:15 +0100

    much improved assembler code, reuses the accumulated data points

--- gzl/mwi.asm
+++ gzl/mwi.asm
@@ -1,18 +1,23 @@
 init:
-        set $sadr, 0            ; set a pointer to where we are in the data set
-begin:	
-	    set $cadr, 0            ; set the address cursor
-        set $acc, 0	            ; reset the accumulator
-        addi $eadr, $sadr, 31   ; calc the end address
-loop:
-	    load $tmp, $cadr        ; load the next data point
+        set $cadr, 0  ; keep a pointer to where we are in the data set
+	    set $acc, 0   ; the accumulator
+
+get_next_point:
+        load $tmp, $cadr        ; load the next data point
         add $acc, $acc, $tmp    ; add the value to the accumulator
         addi $cadr, $cadr, 1    ; inc the address cursor
-        cmp $cadr, $eadr        ; loop if we haven't reached the end address
-        jlt loop
-average:
 	    set $tmp, 1118482       ; div const for 30
-	    div $res, $acc, $tmp    ; divide the accumulator by 30
-        addi $sadr, $sadr, 1    ; inc the start address
-        jmp begin               ; start next mwi calculation
         
+	    div $res, $acc, $tmp    ; divide the accumulator by 30, this is the MWI
+
+        set $tmp, 30            ; check if we have 30 points accumulated
+        cmp $cadr, $tmp
+        jgt align_acc           ; if we do, then align the accumulator
+	    jmp get_next_point      ; if not, load the next data point
+
+align_acc:
+	    set $tmp, 30              ; get the address 30 samples before the current
+        sub $odp_adr, $cadr, $tmp
+        load $odp, $odp_adr       ; load the data point at that address
+        sub $acc, $acc, $odp      ; subtract it's value from the accumulator
+	    jmp get_next_point        ; load the next data point

02805eee0ec8a179142167bbf68b60d5412dabba
Simon Altschuler <simon@altschuler.dk>
2013-11-10 20:11:08 +0100

    assembler implementation

--- gzl/assembly/mwi_test.asm
+++ gzl/assembly/mwi_test.asm
@@ -1,3 +1,10 @@
-sub $r0, $r1, $r2
-
+begin:
+        set $a, 0
+        set $b, 1
+next_fib:
+        add $fib, $a, $b
+        mov $tmp, $a
+        add $a, $a, $b
+        mov $b, $tmp
+        jmp next_fib
         

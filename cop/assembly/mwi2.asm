08d8419d4495d6ab39b529c1c1250768496a9cd5
Simon Altschuler <simon@altschuler.dk>
2013-11-20 20:28:28 +0100

    fixed copro platform integration

--- cop/assembly/mwi2.asm
+++ cop/assembly/mwi2.asm
@@ -1,10 +1,10 @@
 init:
-        set $adr, 0  ; her gemmes seneste adresse som er blevet processeret
+        set $adr, 10  ; her gemmes seneste adresse som er blevet processeret
         set $store_adr, 251
 
 get_next_point:
         load $tmp, $adr        ; load det næste sample fra data memory
-        mwi $res, $tmp         ;
+        mwi $res, $tmp         ; send sample værdi til mwi co-processoren
         store $res, $store_adr ; gem mwi resultatet i data memory
 
         addi $adr, $adr, 1     ; inc adressen
